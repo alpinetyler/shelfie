0ba2d824a2b1f4beee88fa7ec04c2ab23c082967
TylerT <alpinetyler@gmail.com>
2019-06-07 10:49:44 -0600

    working commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require ('express')
+const massive = require('massive')
+require('dotenv').config()
+
 const Ctrl = require('./controller.js')
 
 const app = express()
-const port = 3005
 
 app.use(express.json())
 
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    console.log('connected to db')
+    app.set('db', dbInstance)
+}).catch(err => console.log(err));
 
 
-app.listen(port, () => {
-    console.log('we be listening on port', port)
+
+app.listen(SERVER_PORT, () => {
+    console.log('we be listening on port', SERVER_PORT)
 })
